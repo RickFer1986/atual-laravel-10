2aeac4569ca739305b59b961649af56830830f76
RickFer1986 <ricardo.lazafer@gmail.com>
2023-12-27 21:09:18 -0300

    commit: criacao de rotas

---
+++ resources/views/admin/supports/index.blade.php
@@ -0,0 +1,1 @@
+<h1>Listagem do Suporte</h1>
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,12 @@
 <?php
 
+use App\Http\Controllers\Admin\{SupportController};
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Site\SiteController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
+Route::get('/contatos', [SiteController::class, 'contatos']);
 
 Route::get('/', function () {
     return view('welcome');
 });
-
